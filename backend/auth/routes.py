2d12551a8cefebf1269b561a703a3190b392f825
Anamay-Upreti <rcupretidgc13@gmail.com>
2025-11-08 16:05:39 +0530

    added-router

---
+++ backend/auth/routes.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, HTTPException, Depends, Header
+from ..database import db
+from ..schemas import UserCreate, UserLogin
+from .utils import hash_password, verify_password, create_access_token, decode_access_token
+from bson import ObjectId
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"],
+)
+
+@router.post("/signup")
+def signup(user: UserCreate):
+    users = db["users"]
+    if users.find_one({"email": user.email}):
+        raise HTTPException(status_code=400, detail="Email already exists")
+    user.password = hash_password(user.password)
+    users.insert_one(user)
+    return {"message": "User created successfully"}
+
+@router.post("/login")
+def login(user: UserLogin):
+    users = db["users"]
+    db_user = users.find_one({"email": user.email})
+    if not db_user or not verify_password(user.password, db_user["password"]):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    
+    token = create_access_token({"user_id": str(db_user["_id"])})
+    return {"access_token": token, "token_type": "bearer"}
+
+def get_current_user(Authorization: str = Header(None)):
+    if not Authorization:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+    token = decode_access_token(token)
+    payload = decode_access_token(token)
+    if not payload:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user_id = payload.get("user_id")
+    user = db["users"].find_one({"_id": ObjectId(user_id)})
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
---
+++ backend/notes/routes.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends, HTTPException
+from ..database import db
+from ..schemas import NoteCreate
+from ..auth.routes import get_current_user
+from bson import ObjectId
+
+router = APIRouter(prefix="/notes", tags=["notes"])
+
+@router.post("/")
+def create_note(note: NoteCreate, user = Depends(get_current_user)):
+    notes = db["notes"]
+    new_note = {
+        "title": note.title,
+        "content": note.content,
+        "owner_id": str(user["_id"])
+    }
+    result = notes.insert_one(new_note)
+    return {"id": str (result.inserted_id), **new_note}
+
+@router.get("/")
+def get_notes(user = Depends(get_current_user)):
+    notes = db["notes"].find({"owner_id": str(user["_id"])})
+    return [{"id": str(n["_id"]), "title": n["title"], "content": n["content"]} for n in notes]
+
+@router.put("/{note_id}")
+def update_note(note_id: str, note: NoteCreate, user=Depends(get_current_user)):
+    notes = db["notes"]
+    note_date = notes.find_one({"_id": ObjectId(note_id)})
+    if not note_date or note_date["owner_id"] != str(user["_id"]):
+        raise HTTPException(status_code=403, detail="Note allowed")
+    notes.update_one({"_id": ObjectId(note_id)}, {"$set": {"title": note.title, "content": note.content}})
+    return {"message": "Note updated successfully"}
+
+@router.delete("/{note_id}")
+def delete_note(note_id: str, user=Depends(get_current_user)):
+    notes = db["notes"]
+    note_date = notes.find_one({"_id": ObjectId(note_id)})
+    if not note_date or note_date["owner_id"] != str(user["_id"]):
+        raise HTTPException(status_code=403, detail="Note allowed")
+    notes.delete_one({"_id": ObjectId(note_id)})
+    return {"message": "Note deleted successfully"} 
